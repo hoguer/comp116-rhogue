a05d62af6a774534a13a2c1546cb7c34d2f7adc4
Rachel Hogue <hoguer711@gmail.com>
2015-10-13 18:32:23 -0400

    more progress on assgn 2

--- alarm/README.txt
+++ alarm/README.txt
@@ -1,11 +1,25 @@
 ABOUT
 Identify what aspects of the work have been correctly implemented and what have not:
+    > I attempted to implement all aspects of the work correctly except for the bonus. If I missed something, I am not currently aware of it.
+    
 Identify anyone with whom you have collaborated or discussed the assignment:
     > I worked alone on this assignment.
 
 Say approximately how many hours you have spent completing the assignment:
-    > 4 hours
+    > 10 hours
 
 QUESTIONS
 1.) Are the heuristics used in this assignment to determine incidents "even that good"?
+I don't think they are "even that good." Here are some possible concerns:
+- I don't know that my regex are airtight. For example, if a log for some reason used single quotes (') instead of double quotes ("), or if the payload contained a double quote within it, they would fail to parse the log line correctly.
+- For detecting some of these scans (nikto, masscan, shellshock-scan), I'm simply looking for the names of these in the user agent. I'm not sure if any of these have stealth modes that could bypass detection.
+- I'm not sure if there are other communications that have the same TCP flag settings as the FIN, Xmas, and NULL Nmap scans, which would cause false positives in detecting these scans.
+- My shell code strings probably don't cover all possible shell code strings that you might find in an apache log.
+- The credit card leak check strips out whitespace and dashes, but there could very well be other separators. There may also be other types of credit cards that might not be covered by the regex, and there may be numbers that are identified as credit card numbers that are, in fact, something else. 
+
 2.) If you have spare time in the future, what would you add to the program or do differently with regards to detecting incidents?
+- I would try to close some of the "holes" mentioned above by using existing, effective gems (such as Apache Log Regex, http://simonecarletti.com/blog/2009/02/apache-log-regex-a-lightweight-ruby-apache-log-parser/ as well as looking for credit card verification gems) or by exploring more attacks/incidents that have occurred.
+- I might try to check for Nmap scans that use fragmentation.
+- I would add some error handling
+- I would do the bonus
+- I would check for credit cards in binary/hex
--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 require 'packetfu'
 require 'optparse'
 
 $incident_number = 0
 
-Options = Struct.new(:logfile, :interface, :protocol)
+Options = Struct.new(:logfile, :interface)
 
 class Parser
   def self.parse(options)
@@ -12,7 +13,6 @@ class Parser
 
     #defaults
     args.interface = 'eth0'
-    args.protocol = 'tcp'
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: alarm.rb [options]"
@@ -25,10 +25,6 @@ class Parser
         args.interface = i
       end
 
-      opts.on("-p", "--protocol=NAME", "Name of protocol") do |p|
-        args.protocol = p
-      end
-      
       opts.on("-h", "--help", "Prints this help") do
         puts opts
         exit
@@ -42,74 +38,200 @@ end
 
 options = Parser.parse ARGV
 
+####################################
+# Analyze Web Server Log Functions #
+####################################
+
+# returns true if str_val contains the substring "nmap"
+def contains_nmap?(str_val)
+  return (str_val.downcase.include? "nmap" or
+          str_val.downcase.include? "\6e\6d\61\70")
+end
+
+# returns true if str_val contains the substring "nikto"
+def contains_nikto?(str_val)
+  return (str_val.downcase.include? "nikto" or
+          str_val.downcase.include? "\6e\69\6b\74\6f")
+end
+
+# returns true if str_val contains the substring "masscan"
+def contains_masscan?(str_val)
+  return (str_val.downcase.include? "masscan" or
+          str_val.downcase.include? "\6d\61\73\73\63\61\6e")
+end
+
+# returns true if str_val contains the substring "shellshock-scan"
+def contains_shellshock_scan?(str_val)
+  return (str_val.downcase.include? "shellshock-scan" or
+          str_val.downcase.include? "\73\68\65\6c\6c\73\68\6f\63\6b\2d\73\63\61\6e")
+end
+
+# returns true if str_val contains the substring "phpMyAdmin"
+def contains_phpMyAdmin?(str_val)
+  return (str_val.downcase.include? "phpmyadmin" or
+          str_val.downcase.include? "\70\68\70\6d\79\61\64\6d\69\6e")
+end
+
+# returns true if str_val contains shellcode
+def contains_shell_code?(str_val)
+  shell_strings = ["/bin/bash", "rm -rf", "{ :; }", ".sh"]
+  shell_strings.each do |ss|
+    return true if str_val.include? ss
+  end
+  return false
+end
+
+def parseLogLine(logLine)
+  protocol = "unknown"
+  user_agent = ""
+  
+  ip_match = /^[[0-9].]+/.match(logLine)
+  remaining_str = ip_match.post_match.strip
+  source_ip_addr = ip_match.to_a[0]
+  identity_match = /^(-|[\w.\s]+)/.match(remaining_str)
+  remaining_str = identity_match.post_match.strip
+  userid_match = /^(-|[\w.\s]+)/.match(remaining_str)
+  remaining_str = userid_match.post_match.strip
+  datetime_match = /^\[[^\[\]]+\]/.match(remaining_str)
+  remaining_str = datetime_match.post_match.strip
+  payload_match = /^"[^"]*"/.match(remaining_str)
+  payload = payload_match.to_a[0]
+  if not payload.empty? and not payload == "\"\""
+    protocol_match = /\s[^\s"]+"$/.match(payload)
+    protocol_match_arr = protocol_match.to_a
+    if not protocol_match_arr[0].nil?
+      protocol = protocol_match_arr[0].strip.chomp("\"")
+    end
+  end
+
+  remaining_str = payload_match.post_match.strip
+  status_code_match = /^\d{3}/.match(remaining_str)
+  remaining_str = status_code_match.post_match.strip
+  resp_byte_count_match = /^(-|[0-9]+)/.match(remaining_str)
+  remaining_str = resp_byte_count_match.post_match.strip
+  
+  # Combined log format has two addtl pieces of information:
+  #  1. referer and 2. user agent
+  if not remaining_str.empty?
+    referer_match = /^"[^"]+"/.match(remaining_str)
+    remaining_str = referer_match.post_match.strip
+    user_agent_match = /^"[^"]+"/.match(remaining_str)
+    user_agent = user_agent_match.to_a[0]
+    remaining_str = user_agent_match.post_match.strip
+  end
+  
+  return source_ip_addr, payload, protocol, user_agent
+  
+end
+
 ####################################################
 # Analyze Live Stream of Network Packets Functions #
 ####################################################
 
 # returns true if the packet is from a NULL scan
 def is_null_scan?(packet)
-  return packet.tcp_header.tcp_flags.to_i == 0
+  return (packet.is_tcp? and (packet.tcp_header.tcp_flags.to_i == 0))
 end
 
 # returns true if the packet is from a FIN scan
 def is_fin_scan?(packet)
-  return packet.tcp_header.tcp_flags.to_i == 1
+  return (packet.is_tcp? and (packet.tcp_header.tcp_flags.to_i == 1))
 end
 
 # returns true if the packet is from an Xmas scan
 def is_xmas_scan?(packet)
-  return packet.tcp_header.tcp_flags.to_i == 41
+  return (packet.is_tcp? and (packet.tcp_header.tcp_flags.to_i == 41))
 end
 
 # returns true if the packet is from another Nmap scan
 def is_nmap_scan?(packet)
-  payload = packet.tcp_header.body
-  return payload.downcase.include? "nmap"
+  return (contains_nmap?(packet.tcp_header.body) or
+          contains_nmap?(packet.payload))
 end
 
-#Nikto scan
-def is_nikto_scan?
-  return false
+# returns true if the packet is from a Nikto scan
+def is_nikto_scan?(packet)
+  return contains_nikto?(packet.payload)
 end
 
-#Credit card leak
-def is_credit_card_leak?
-  #http://www.richardsramblings.com/regex/credit-card-numbers/
-  return false
-end
+# returns true if the packet contains a credit card leak
+def is_credit_card_leak?(packet)
+  check_str = packet.payload
+  check_str.tr!('-', '')
+  check_str.gsub!(/\s+/, '')
 
-####################################
-# Analyze Web Server Log Functions #
-####################################
+  cc_regex = "(?:" +
+              "4[0-9]{12}(?:[0-9]{3})?"        +   # Visa
+             "|5[1-5][0-9]{14}"                +   # MasterCard
+             "|3[47][0-9]{13}"                 +   # American Express
+             "|3(?:0[0-5]|[68][0-9])[0-9]{11}" +   # Diners Club
+             "|6(?:011|5[0-9]{2})[0-9]{12}"    +   # Discover
+             "|(?:2131|1800|35\d{3})\d{11}"    +   # JCB
+             ")"
 
-#TBD
+  cc_match = /#{cc_regex}/.match(check_str)
+  return (not cc_match.to_a[0].nil?)
+end 
 
 def reportIncident(incident, source_ip, protocol, payload)
    $incident_number += 1
    puts "#{$incident_number}. ALERT: #{incident} is detected from #{source_ip} (#{protocol}) (#{payload})!"
 end
 
+##############
+# BEGIN MAIN #
+##############
+
 if options.logfile
-  #TBD
-else
-  cap = PacketFu::Capture.new(:iface => options.interface, :start => true, :promisc => true, :filter => options.protocol)
+  File.readlines(options.logfile).each do |line|
+    source_ip_addr, payload, protocol, user_agent = parseLogLine(line)
+    incident = "None"
+
+    if contains_phpMyAdmin?(payload)
+      incident = "Something involving phpMyAdmin"
+    elsif not user_agent.empty?
+      if contains_nmap?(user_agent)
+        incident = "Nmap Scan"
+      elsif contains_nikto?(user_agent)
+        incident="Nikto Scan"
+      elsif contains_masscan?(user_agent)
+        incident="Masscan"
+      elsif contains_shellshock_scan?(user_agent)
+        incident="Shellshock Vulnerability Scan"
+      elsif contains_shell_code?(user_agent)
+        incident="Somthing involving shellcode"
+      end
+    end
+
+    if incident != "None"
+      reportIncident(incident, source_ip_addr, protocol, payload)
+    end
+    
+  end
+  
+else #live capture
+  cap = PacketFu::Capture.new(:iface => options.interface, :start => true, :promisc => true)
   cap.stream.each do |p|
     incident = "None"
     pkt = PacketFu::Packet.parse(p)
 
-    if is_null_scan?(pkt)
+    if is_nikto_scan?(pkt)
+      incident = "Nikto Scan"
+    elsif is_null_scan?(pkt)
       incident = "NULL Scan"
     elsif is_fin_scan?(pkt)
       incident = "FIN Scan"
     elsif is_xmas_scan?(pkt)
       incident = "Xmas Scan"
+    elsif is_nmap_scan?(pkt)
+      incident = "Nmap Scan"
+    elsif is_credit_card_leak?(pkt)
+      incident = "Credit Card Leak"
     end
     
     if incident != "None"
-      reportIncident(incident, pkt.ip_saddr, options.protocol, pkt.tcp_header.body)
+      reportIncident(incident, pkt.ip_saddr, pkt.proto.last, pkt.payload)
     end
+    
   end
 end
-
-#stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
-#stream.show_live()
